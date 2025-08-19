ab5c8f0c43f2bcda146f23548519c3046ae53ef1
jarretphon <jarretphon@gmail.com>
2025-08-19 15:29:47 +0800

    Implement chatbot skeleton to greet user and exit

--- src/main/java/Xenon.java
+++ src/main/java/Xenon.java
@@ -1,6 +1,20 @@
 public class Xenon {
     public static void main(String[] args) {
+        greet();
+        exit();
+    }
 
+    public static void greet() {
+        System.out.println("----------------------------------------------");
+        System.out.println("Hello! I'm Xenon");
+        System.out.println("What can I do for you?");
+        System.out.println();
+    }
 
+    public static void exit() {
+        System.out.println("----------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println();
+        System.out.println("----------------------------------------------");
     }
 }
